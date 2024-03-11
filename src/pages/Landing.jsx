3a39fc738ff13a302f173f62183542e0aa8d6b45
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-03-11 10:14:13 +0700

    - update landing

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,8 @@
-import { Logo } from '../components'
-import main from '../assets/images/main.svg'
-import { Link } from 'react-router-dom'
-import Wrapper from '../assets/wrappers/LandingPage'
+import styled from 'styled-components';
+import Wrapper from '../assets/wrappers/LandingPage';
+import main from '../assets/images/main.svg';
+import { Link } from 'react-router-dom';
+import { Logo } from '../components';
 
 const Landing = () => {
   return (
@@ -9,8 +10,8 @@ const Landing = () => {
       <nav>
         <Logo />
       </nav>
-      <div className="container page">
-        <div className="info">
+      <div className='container page'>
+        <div className='info'>
           <h1>
             job <span>tracking</span> app
           </h1>
@@ -20,17 +21,17 @@ const Landing = () => {
             quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
             narwhal.
           </p>
-          <Link to="/register" className="btn register-link">
+          <Link to='/register' className='btn register-link'>
             Register
           </Link>
-          <Link to="/login" className="btn ">
+          <Link to='/login' className='btn '>
             Login / Demo User
           </Link>
         </div>
-        <img src={main} alt="job hunt" className="img main-img" />
+        <img src={main} alt='job hunt' className='img main-img' />
       </div>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
